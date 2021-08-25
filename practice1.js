6b5a3c64f5f02e0f6414727254fa88f1c87e2aea
hossain377 <hossainahmed377@gmail.com>
2021-08-25 20:46:31 +0600

    first commit

---
+++ practice1.js
@@ -0,0 +1,37 @@
+//the calculation of the circle area
+
+var radius = 7;
+const pi = 3.1416;
+var areaOfCircle = pi * (radius * radius);
+console.log("total area of the circleis ", areaOfCircle, "square inch");
+
+console.log("hossain");
+
+//calculation of bdt to usd 
+
+var usdToBdt = 80;
+var bdt = 5000;
+var usd = bdt / usdToBdt;
+console.log("$", usd);
+
+//finding reminder
+/*
+var n1 = 5;
+var r1 = 5 % 2;
+console.log("the reminder is", r1);
+*/
+
+var x;
+var y = 5;
+x = ++y;
+//y=y+1;
+//x=y;
+console.log("x=",x, "y=",y);
+//assignment variable
+var x=4;
+//x +=2; //x = x +2
+//x -=1; //x = x - 1;
+//x *=2; //x = x * 2;
+//x /=2; //x = x / 2;
+x %=3; // x = x % 3;
+console.log(x);
