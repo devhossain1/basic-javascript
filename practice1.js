f1a743a2952a799bc8c8ecc37f8195bdde710784
hossain377 <hossainahmed377@gmail.com>
2021-08-28 13:17:00 +0600

    while, for loop

--- practice1.js
+++ practice1.js
@@ -1,115 +1,76 @@
 
+//ternary operator
+var n=-5;
+var result;
+//var remainder = n % 2;
+//result = (0 == remainder) ? "even" : "odd" ;
+//console.log("This is the number", result);
+result= (n<0) ? "negetive" : "positive";
+console.log("this is the ", result, "number");
+
+//while loop
+//var i=0;
 /*
-var n=8;
-if(8==n){
-    console.log("n is equal to 8");
-}else{
-    console.log("n is not equal to 8");
+while(true){
+    i++;
+    console.log(i);
+    if(9 == i){
+        break;
+    }
+
 }
-console.log(n);
 */
-
-var personOne = "jamal";
-var personTwo = "kamal";
-var areTheyBrother = true;
-
 /*
-if("jamal"==personOne){
-    if("kamal"==personTwo){
-        if(areTheyBrother){
-          console.log("jamal & kamal are brother");  
-        }else{
-            console.log("jamal & kamal are not brother");
-        }
+while(i<=10){
+    console.log(i);
+    i+=2;
+}
+*/
+/*
+var i = 0;
+
+while(i++ < 5){
+    console.log(i);
+}
+
+console.log("\n reset \n");
+i=0;
+while(++i < 5){
+    console.log(i);
+}
+*/
+/*
+var i=0;
+do{
+    i++;
+    console.log(i);
+    if(10==i){
+        break;  
+    }
+}while(true);
+*/
+console.log("\n another \n");
+
+//for loop
+//for(initialization; condition; steping){}
+//infinite loop: for (;;){ }
+/*
+var i;
+i = 0;
+
+for(;;){
+    i++;
+    console.log(i);
+    if(8==i){
+        break;
     }
 }
 */
-if("jamal"==personOne && "kamal" == personTwo && areTheyBrother){
-    console.log("jamal & kamal are brother");
-}else{
-    console.log("they are not brother");
-}
-//2357
-//var n = 7;
-//nesting method
-/* 
-if(n < 10){
-    if(2==n || 3==n || 5==n || 7==n){
-       console.log("this number", n, "is smaller than 10 and it's a prime number");
-    }else{
-        console.log("this number", n, "is smaller than 10 and it's not a prime number");
-    }
-}else{
-    console.log("this number", n, "is greater than 10 ");
-}
-*/
-// without nesting method
-//var n = 22;
-/*
-if( n<10 && (2==n || 3==n || 5==n || 7==n)){
-    console.log("this number", n, "is smaller than 10 and it's a prime number");
- }else if(n < 10){
-     console.log("this number", n, "is smaller than 10 and it's not a prime number");
- }else{
-     console.log("this number ", n, "is greater than 10");
- }
- */
-/*
-var n = -3;
-if(n>0){
-    var result = "this number " + n + " it is positive"
-}else{
-    var result = "this number " + n + " it is negetive"
-}
-console.log(result);
-*/
-/*
-var n=0;
-var result;
-var reminder = n % 2;
-if(0==n){
-    result ="this is the neutral number";
-}else if(0 == reminder){
-    result = "this number is even";
-}else{
-    result = " this number is odd";
-}
-console.log(result);
-*/
-var movieRating = "r";
-var age = 53;
-/*
-if("pg"== movieRating && age >=13){
-    console.log("you can watch this movie");
-}else if("r"==movieRating && age >= 18){
-    console.log("you can watch this movie");
-}else if("g"==movieRating){
-    console.log("you can watch this movie");
-}else{
-   console.log("you can not watch this video")
-}
-*/
+console.log("\n another \n");
 
-if(("pg"== movieRating && age >=13) || ("r"==movieRating && age >= 18) || ("g"==movieRating)){
-    console.log("you can watch the movie");
-}else{
-    console.log("you can not watch the movie"); 
+var i,j;
+
+for(i=0,j=10; i<=10; i++,j--){
+    console.log(i,j);
 }
-// baby, tadler,teanage, adult
-var yourAge = -1;
-var result;
-if(yourAge<0 || yourAge>200){
-    result = "vampire";
-}else if(yourAge<=1){
-  result = "baby";
-}else if(yourAge<=3){
-    result = "taddler";
-}else if(yourAge<=12){
-    result = "kid";
-}else if(yourAge<18){
-    result = "teenager";
-}else if(yourAge>18){
-    result = "adult";
-}
-console.log("you are a", result);
 
