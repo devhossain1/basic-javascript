53fbde399d84317a8dc6b3c201c3003efb9d52d3
hossain377 <hossainahmed377@gmail.com>
2021-09-03 00:26:32 +0600

    split join

--- practice1.js
+++ practice1.js
@@ -1,75 +1,4 @@
 
-
-//array 
-var fruits = ["apple","banana","orange", "grape"];
-console.log(fruits[4]);
-
-fruits[4]="mango";
-console.log(fruits[4]);
-
-console.log(fruits.length);
-var lastOffset = fruits.length-1;
-console.log(fruits[lastOffset]);
-
-var myList=["apple","orange","banana"];
-myList[myList.length]="grape";
-myList.push("pineapple");
-myList.unshift("mangoes");
-console.log(myList.length);
-var lastElement=myList.pop();
-
-var firstElement=myList.shift();
-console.log(myList.length);
-console.log(myList);
-console.log(lastElement);
-console.log(firstElement);
-
-var males= ["rahim","karim","shamima", "salek"];
-var females=["rahima","karima"];
-//var lastPerson=males.pop();
-//females.push(lastPerson);
-var personRemove=males[2];
-females.push(personRemove)
-
-delete(males[2]);
-//males.splice(2,1);
-
-console.log(males,females);
-console.log(males.length, females.length);
-var list=[
-    "sun",//0 = -7
-    "mon",//1 = -6
-    "tue",//2 = -5
-    "wed",//3 = -4
-    "thu",//4 = -3
-    "fri",//5 = -2
-    "sat",//6 = -1
-   ];
-   //var chunk=list.slice(4,6);
-   //console.log(list, chunk);
-
-   //var length= list.length-3;
-   //var chunk=list.slice(length);
-   var chunk= list.splice(-4,2);
-
-   console.log(list, chunk);
-
-   var list1 = ["apple","mon","tue","wed"];
-
-   var list2=list1.slice();
-   //var list2 = [...list1];
-   //var list2 = Array.from(list1);
-   list2[1] = "mangoes";
-   console.log(list1,list2);
-
-   var type1 = ["jan","feb"];
-   var type2 =  ["mar","apr"];
-   var type3 = ["may", "jun"];
-
-   //var type = type1.concat(type2,type3);
-   var type = [].concat(type1,type2,type3);
-   console.log(type);
-
    var list=[
     "sun",//0 = -7
     "mon",//1 = -6
@@ -88,3 +17,39 @@ var list=[
 for(i in list){
     console.log("element at", i, "offset of",list[i]);
 }
+
+//var list=["mangoes", "orange", "apple", "banana","grape"];
+var list=[3,34,11,2,23,1,4,43,52,5];
+
+var length = list.length-1;
+for(var i=0; i<length; i++){
+    for(var j=0; j<length; j++){
+        if(list[j] > list[j+1]){
+            [list[j],list[j+1]]= [list[j+1],list[j]];
+        }
+    }
+}
+//list.sort();
+console.log(list);
+/*
+var s = "hello world how are you";
+var a= s.split(" ");
+console.log(a);
+ var ns = a.join("-")
+ console.log(ns);
+ console.log("he is a good boy".split(" ").join("-"));
+ */
+var n = "hossain ahmed";
+var rev = n.split("").reverse().join("");
+console.log(rev);
+
+var a = "quick brown fox jump over the lazy dogs";
+var b = a.split(" ");
+console.log(b);
+var c = b.length;
+
+var o = a.split("0")
+console.log(o.length);
+
+console.log(c);
+console.log(a.split("").length);
