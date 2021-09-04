03f48b20033fa8b4fe9253094dbea5692300a4ea
hossain377 <hossainahmed377@gmail.com>
2021-09-04 07:44:37 +0600

    escope spread operator

--- practice1.js
+++ practice1.js
@@ -1,36 +1,4 @@
 
-   var list=[
-    "sun",//0 = -7
-    "mon",//1 = -6
-    "tue",//2 = -5
-    "wed",//3 = -4
-    "thu",//4 = -3
-    "fri",//5 = -2
-    "sat",//6 = -1
-    "apple"
-   ];
-
-   var length = list.length;
-//for(var i=0; i<length; i++){
- //   console.log("element at", i, "offset of",list[i]);
-//}
-for(i in list){
-    console.log("element at", i, "offset of",list[i]);
-}
-
-//var list=["mangoes", "orange", "apple", "banana","grape"];
-var list=[3,34,11,2,23,1,4,43,52,5];
-
-var length = list.length-1;
-for(var i=0; i<length; i++){
-    for(var j=0; j<length; j++){
-        if(list[j] > list[j+1]){
-            [list[j],list[j+1]]= [list[j+1],list[j]];
-        }
-    }
-}
-//list.sort();
-console.log(list);
 /*
 var s = "hello world how are you";
 var a= s.split(" ");
@@ -38,7 +6,7 @@ console.log(a);
  var ns = a.join("-")
  console.log(ns);
  console.log("he is a good boy".split(" ").join("-"));
- */
+ 
 var n = "hossain ahmed";
 var rev = n.split("").reverse().join("");
 console.log(rev);
@@ -48,8 +16,89 @@ var b = a.split(" ");
 console.log(b);
 var c = b.length;
 
-var o = a.split("0")
-console.log(o.length);
+var o = a.split("o")
+console.log(o.length-1);
 
 console.log(c);
 console.log(a.split("").length);
+*/
+// function
+
+function sayHi(name) {
+    if(name){
+        console.log("Hello " + name);
+    }else{
+        console.log("How are you");
+    }
+    
+}
+sayHi("Hossain");
+sayHi("Ahmed");
+sayHi("");
+sayHi();
+
+function isEven(number){
+    if(number % 2==0){
+        return true;
+    }else{
+        return false;
+    }
+}
+console.log(isEven(12));
+console.log(isEven(13));
+console.log(isEven(32));
+
+function sayHello(name = "Anonymus", age= "unknown") {
+    console.log("my name is " + name + " i am " + age + " years old");
+}
+sayHello("Hossain", "28");
+sayHello("ahmed","22");
+sayHello();
+
+function reverseString(data){
+    var reversedString= data.split('').reverse().join('');
+    return reversedString;
+}
+var sentense="hello world";
+var sentense2="hello world";
+var reversedSentense= reverseString(sentense);
+var reversedSentense2= reverseString(sentense2);
+console.log(reversedSentense, reversedSentense2);
+
+var students = [];
+addStudents(students,"rahim","karim","shamim");
+console.log(students.length);
+addStudents(students,"Barek");
+console.log(students.length);
+addStudents(students,"khalek","Ata");
+console.log(students.length);
+
+//... spread operator
+function addStudents(storage, ...people){
+    for(i=0; i<people.length; i++){
+        storage.push(people[i]);
+    }
+}
+/*
+function dummy(m,n, ...params){
+    console.log(params.length);
+    console.log(params);
+    //console.log(params[1]);
+}
+dummy(1,3, 6, 7);
+dummy(1,5, 4, 8,9,3);
+dummy(1,3, 6, 7);
+*/
+
+var i = 35;
+function doSomething(){
+        let i = 23;
+    if(true){
+      let i = 10;
+
+      console.log("inside if block",i);
+    }
+    console.log("inside the function", i);
+}
+doSomething();
+console.log("outside the function", i);
