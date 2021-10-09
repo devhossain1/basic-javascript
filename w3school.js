7ff964c10885e7b442e20357ff056f9c7baa4dc8
hossain377 <hossainahmed377@gmail.com>
2021-10-09 19:27:00 +0600

    problem solve 62

--- w3school.js
+++ w3school.js
@@ -1,48 +1,101 @@
 
-// //  let date = new Date();
-// //  console.log(date.getFullYear());
+const sentence = " We know Lorem Ipsum is simply dummy text of the lorem ipsum \
+system and management. it is the text of lorem ipsum organization";
 
-//  class Car {
-//     constructor(name, year) {
-//       this.name = name;
-//       this.year = year;
-  
-//     }
-//     age() {
-//       let date = new Date();
-//       return date.getFullYear() - this.year;
-//     }
-//   }
-  
-//   let myCar = new Car("Ford", 2014);
-//   let myBus = new Car("BMW", 2010);
-//   document.getElementById("demo").innerHTML =
-//   "My car is " + myBus.age() + " years old.";
+const matches = sentence.match(/lorem/ig);
+const occarance = matches ? matches.length : 0;
 
-//   let text = '{ "employees" : [' +
-// '{ "firstName":"John" , "lastName":"Doe" },' +
-// '{ "firstName":"Anna" , "lastName":"Smith" },' +
-// '{ "firstName":"Peter" , "lastName":"Jones" } ]}';
+let position = sentence.search(/lorems/i);
+position = position >=0 ? position :"not found !!";
 
-// const obj = JSON.parse(text);
-// console.log(obj);
+console.log(occarance);
+console.log(position);
 
-// console.log(JSON.stringify(obj));
-
-class Person {
-    constructor(name,age){
-        this.name = name;
-        debugger;
-        this.age = age;
+//linearSearch(['a','b', 'c', 'd', 'e'], 'c');
+function linearSearch(arr, val){
+    const len = arr.length;
+    for(let i = 0; i < len; i++){
+        if(arr[i] === val){
+            return i;
+        }
     }
-    play(status){
-        debugger;
-        console.log(`${this.name}  aged ${this.age} is played ${status}`)
+    return 'not found !';
+}
+console.log(linearSearch(['a','b', 'c', 'd', 'e'], 'c'));
+//anather problem solving
+
+function longestString(names){
+    let longestWord = '';
+
+    for(nameval of names){
+        if(nameval.length > longestWord.length){
+            longestWord = nameval;
+        }
+  }
+  return [longestWord, names.indexOf(longestWord)]
+}
+  
+
+console.log(longestString(['sumit saha', 'lern with sumit', 'hossain', 'ahmed']));
+
+//problelm solve number which is divisible by 3,5 and both 3&5
+
+function fizzBuzz(number){
+   
+    for(let i=1; i <= number; i++){
+        if(i % 15==0){
+            console.log(`${i} is fizzBuzz`);
+        }else if(i % 3 == 0){
+            console.log(`${i} is fizz`);
+        }else if(i % 5 == 0){
+            console.log(`${i} is Buzz`);
+        }else{
+            console.log(i);
+        }
     }
 }
-debugger;
-const person1= new Person("shakib");
-const person2= new Person("tamim", 37);
+fizzBuzz(46);
 
-person1.play("well");
-person2.play("bad");
+//how can i find out falsey value from an array ?
+//falsy value = false, NaN, '', undefined, 0
+
+const mixedArr = [
+    'lws',
+    undefined,
+    'hossain', 
+    false, 
+    "", 
+    "learn with sumit",
+    40,
+    "k",
+    true,
+    "thanks all",
+    NaN
+];
+
+const trueArray = mixedArr.filter(Boolean)
+console.log(trueArray);
+
+const obj = {
+    a: 'lws',
+    b: undefined,
+    c: 'hossain', 
+    d: false, 
+    e: "", 
+    f: "learn with sumit",
+    g: 40,
+    h: "k",
+    i: true,
+    j: "thanks all",
+    k: NaN
+};
+const truthyObject = function(obj){
+    for(let i in obj){
+        if(!obj[i]){
+            delete obj[i];
+        }
+       
+    }
+    return obj;
+}
+console.log(truthyObject(obj));
