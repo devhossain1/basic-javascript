56ffa198c7aefcf591b4b3e835468beb21cc2dd7
hossain377 <hossainahmed377@gmail.com>
2021-10-19 18:56:00 +0600

    project of addEventListenner

--- w3school.js
+++ w3school.js
@@ -1,24 +1,55 @@
+// let len = document.querySelectorAll(".myButton").length;
 
-let myVar = document.getElementById('para2Id');
-function addStyle(){
-  myVar.classList.add('para-style');
+// for(let i=0; i<len; i++){
+//   document.querySelectorAll(".myButton")[i].addEventListener("click",function(){
+//     let text = this.innerHTML;
+//     document.querySelector("h1").innerHTML = text + " is clicked";
+//   }); 
+// }
+
+for(let i=0; i<3; i++){
+  document.querySelectorAll(".myButton")[i].addEventListener("click", function(){
+    let text = this.innerHTML;
+    console.log(text);
+    audioPlay(text);
+    playAnimation(text);
+
+  });
 }
 
-function removeStyle(){
-  myVar.classList.remove('para-style');
+document.addEventListener("keypress", function(event){
+  var text = event.key;
+  audioPlay(text);
+  playAnimation(text);
+  
+});
+
+function audioPlay(text){
+  switch(text){
+    case "a":
+      var song = new Audio("songs/Juboti-radhe.mp3");
+      song.play();
+      break;
+    case "b":
+      var song = new Audio("songs/paramSundori.mp3");
+      song.play();
+      break;
+    case "c":
+      var song = new Audio("songs/Juboti-radhe.mp3");
+      song.play();
+      break;
+  };
+
+
 }
 
-document.querySelector("#button3").addEventListener("click", function(){
-  alert (" Hello everybody !!");
-});
+function playAnimation(text){
+  var selectedButton = document.querySelector("." + text);
+  selectedButton.classList.add("anim");
 
+  setTimeout(function(){
+    selectedButton.classList.remove("anim");
 
+  },2000);
 
-let bangla = document.querySelector('h1');
-
-bangla.addEventListener("mouseover", function(){
-  bangla.classList.add('my-style');
-});
-bangla.addEventListener("mouseout", function(){
-  bangla.classList.remove('my-style');
-})
+}
