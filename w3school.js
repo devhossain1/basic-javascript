e5c0f5125d1b7e4791d0205a4f4c1f28a39d8929
hossain377 <hossainahmed377@gmail.com>
2021-10-21 08:01:28 +0600

    clock making

--- w3school.js
+++ w3school.js
@@ -1,82 +1,28 @@
-// var locationDiv= document.querySelector(".location-div");
-// console.log(locationDiv);
-
-// var p1 = locationDiv.children[0];
-// p1.textContent = location.href; 
-
-// var p2 = locationDiv.children[1];
-// p2.textContent = location.protocol;
-
-// var p3 = locationDiv.children[2];
-// p3.textContent = location.hostname;
-
-// var p4 = locationDiv.children[3];
-// p4.textContent = location.port;
-
-// let visitButton = document.getElementById("visit-button");
-// visitButton.addEventListener("click", function(){
-//   location.assign("https://devhossain.com");
-// })
-
-// function deleteSomething(){
-//   let value = confirm("Do you want to delete?");
-//   if(value){
-//     console.log("deleted");
-//   }else{
-//     console.log("not deleted");
-//   }
-// }
-// deleteSomething();
-
-// function welcomeMessage(){
-//   let h1 = document.createElement("h1");
-//   let text;
-  
-//   let name = prompt("enter your name");
-//   if(name == null || name == ''){
-//     text = "no name found";
-//   }else{
-//     text = " welcome " + name;
-//   }
-
-//   var textNode= document.createTextNode(text);
-//   h1.appendChild(textNode);
-//   document.body.appendChild(h1);
-
-
-// }
-// welcomeMessage();
-
-// setTimeout(()=>{
-//   console.log("hello")
-// },2000);
-
-// setTimeout(display, 2000);
-// function display(){
-//   console.log("display timeout");
-// }
-
 let saveButton = document.querySelector(".save-btn");
 let message = document.querySelector(".message");
 
-// saveButton.addEventListener("click", saveUser);
+saveButton.addEventListener("click", startClock);
 
-// function saveUser(){
-//   message.textContent = "user registration successfull !";
+function startClock(){
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
 
-//   setTimeout(()=>{
-//     message.textContent = "";
-//   }, 2000);
+  minutes=formatTime(minutes);
+  seconds = formatTime(seconds);
 
-// }
-saveButton.addEventListener("click", saveCount);
+  let time = hours+ ":"+ minutes + ":" + seconds;
 
-function saveCount(){
-  let count = 0;
-  message.textContent = count;
+  message.textContent = time;
+  
+  setInterval(startClock, 1000);
 
-  setInterval(()=>{
-    count++;
-    message.textContent = count;
-  },1000);
 }
+function formatTime(value){
+  if(value<10){
+    value = "0" + value;
+  }
+  return value;
+}
+
