c469da2a770f904a0b01436a108be21b45dc5c50
hossain377 <hossainahmed377@gmail.com>
2021-10-22 12:05:20 +0600

    array & object destructuring

--- w3school.js
+++ w3school.js
@@ -37,3 +37,43 @@ function studentsName1(){
 const studentsName2 = ()=> students.filter((x) => x.gpa > 3).map((y) => y.name);
 
 console.log(studentsName2());
+
+//array destructing
+let numbers = [5,7,8,21,11];
+let [num1,num2,num3, ...z]= numbers;
+console.log(num1);
+console.log(num2);
+console.log(z);
+
+let a=23, b=12;
+[a,b]=[b,a]
+console.log(a);
+
+//object destructing
+const studentsInfo ={
+  id:34,
+  fullName:"hossain ahmed",
+  gpa:3.44,
+  language: {
+    native:"bangla",
+    beginer:"english"
+  }
+}
+
+let {id,fullName,gpa, language} = studentsInfo;
+console.log(fullName);
+console.log(id);
+console.log(language.native);
+
+//destracturing function parameters
+
+const studentFunc =({id,fullName})=>{
+
+console.log(`${id} , ${fullName}`);
+}
+
+let student = {
+  id:322,
+  fullName:"hossain"
+}
+studentFunc(student);
