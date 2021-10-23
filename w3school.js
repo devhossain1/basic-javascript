4da5f1840d0525b8bde22e487a31215f1b3e959c
hossain377 <hossainahmed377@gmail.com>
2021-10-23 12:50:40 +0600

    async & await

--- w3school.js
+++ w3school.js
@@ -1,132 +1,89 @@
-//
-// const numbers = [5,15,32,22,12];
+console.log("welcome");
+//how to create promise - pending , resolve, reject
 
-// const evenNum = (value,index, array) =>{
-//   if(value%2===0)
-//   return value;
-// }
-// let firstEvenNumIndex = numbers.findIndex(evenNum);
-// console.log(firstEvenNumIndex);
-
-// let firstEvenNum = numbers.find(evenNum);
-// console.log(firstEvenNum);
-
-// const students = [
-//   {
-//     id:23,
-//     gpa:3.50
-//   },
-//   {
-//     id:24,
-//     gpa:3.20
-//   },
-//   {
-//     id:27,
-//     gpa:5.50
-//   },
-//   {
-//     id:28,
-//     gpa:4.50
+// const promise1 = new Promise((resolve, reject) =>{
+//   let completedPromise = true;
+//   if(completedPromise){
+//     resolve('completed promise 1');
+//   }else{
+//     reject(new Error('not completed promise 1'));
 //   }
-// ];
-// console.log(students.find(x=>x.gpa>4));
+// });
 
-// const message = "Today is friday";
-// console.log(message.includes("friday"));
+// const promise2 = new Promise((resolve, reject) =>{
+//   resolve("completed promise 2");
+// });
+// //console.log(promise1);
+// // promise1.then(res =>{
+// //   console.log(res);
+// // });
+// // promise1.catch(err =>{
+// //   console.log(err.message);
+// // })
 
-
-//  import {text as message} from './myModule.js'
-
-// console.log(message);
-// setText("good bye from es6");
-// console.log(text);
-
-// class Students {
-//   constructor(id, name){
-//     this.id = id;
-//     this.name = name;
-//   }
-
-//   set studentName(name){
-//     this.name = name;
-//   }
-
-//   get studentInfo(){
-//     return this.id + " " + this.name;
-//   }
-
-// }
-// let s1 = new Students(101, "ahossain");
-// console.log(s1);
-// console.log(s1.id);
-// console.log(s1.name);
-// s1.studentName = "hossainahmed";
-// console.log(s1.name);
-// console.log(s1.studentInfo);
-
-// const taskOne = ()=>{
-//   console.log("task1");
-// };
-
-
-// const taskTwo = ()=>{
+// // promise2.then(res => console.log(res));
+// Promise.all([promise1, promise2]).then(([res1,res2]) => console.log(res1,res2));
+//const promise1 = new Promise((resolve, reject) =>{
 //   setTimeout(()=>{
-//     console.log("task2, dataloading");
-//   }, 2000);
-// };
-// const taskThree = ()=>{
-//   console.log("task3");
-// };
-// const taskFour = ()=>{
-//   console.log("task4");
-// };
+//     resolve("completed promise 1");
+//   },2000)
+// });
 
-// taskOne();
-// taskTwo();
-// taskThree();
-// taskFour();
-function square(x){
-  console.log(`square of ${x} : ${x*x}`);
+// const promise2 = new Promise((resolve, reject) =>{
+//    setTimeout(()=>{
+//      resolve("completed promise 2");
+//    },1000)
+//  });
 
-}
-// const y = square;
-// y(5);
-// function higherOrderFunction(num,callback){
-//   callback(num);
-// }
-// higherOrderFunction(6,square)
+//  Promise.race([promise1, promise2]).then((res) => console.log(res));
 
-const taskOne = (callback)=>{
-  console.log("task1");
-  callback();
-};
-
-
-const taskTwo = (callback)=>{
-  setTimeout(()=>{
-    console.log("task2, dataloading");
-    callback();
-  }, 2000);
-};
-const taskThree = (callback)=>{
-  console.log("task3");
-  callback();
-};
-const taskFour = ()=>{
-    console.log("task4");
-  };
-
-taskOne(()=>{
-  taskTwo(()=>{
-    taskThree(()=>{
-      taskFour();
-
-    });
+const taskOne = ()=>{
+  return new Promise((resolve,reject)=>{
+    resolve('task 1 is completed');
   });
-});
+}
+const taskTwo = ()=>{
+  return new Promise((resolve,reject)=>{
+    resolve('task 2 is  completed');
+  });
+}
+const taskThree = ()=>{
+  return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+      reject('task 3 is not completed');
+    },2000)
+    
+  });
+}
+const taskFour = ()=>{
+  return new Promise((resolve,reject)=>{
+    resolve('task 4 is completed');
+  });
+}
 
-console.log("hello");
-document.querySelector("button").addEventListener("click", ()=>{
-  console.log(" the button is clicked");
-});
-console.log("bye");
+// taskOne()
+// .then((res)=>console.log(res))
+// .then(taskTwo)
+// .then((res)=>console.log(res))
+// .then(taskThree)
+// .then((res)=>console.log(res))
+// .then(taskFour)
+// .then((res)=>console.log(res))
+// .catch((err)=>console.log(err));
+const callAllTasks=async()=>{
+  try{
+    const t1 = await taskOne();
+  console.log(t1);
+  const t2 = await taskTwo();
+  console.log(t2);
+  const t3 = await taskThree();
+  console.log(t3);
+  const t4 = await taskFour();
+  console.log(t4);
+  }catch(error){
+    console.log(error);
+  }
+}
+callAllTasks();
+
+console.log("ends");
